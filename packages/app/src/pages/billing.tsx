888168a7aac585223bb6aae3924eae4450e5b920
Benrobo <alumonabenaiah71@gmail.com>
2023-09-27 21:15:54 +0100

    added major updates

---
+++ packages/app/src/pages/billing.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+function Billing() {
+  return (
+    <Layout activePage="billing">
+      <div className="w-full">Billing</div>
+    </Layout>
+  );
+}
+
+export default Billing;
