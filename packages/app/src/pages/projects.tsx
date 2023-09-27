375caf8d813e8c10442c6ed8d1b862bf2804164e
Benrobo <alumonabenaiah71@gmail.com>
2023-09-27 21:16:01 +0100

    added major updates

---
+++ packages/app/src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+function Projects() {
+  return (
+    <Layout activePage="projects">
+      <div className="w-full">Projects</div>
+    </Layout>
+  );
+}
+
+export default Projects;
