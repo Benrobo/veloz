20850ed4ac60f3b139207f1342566aa5af042f5d
Benrobo <alumonabenaiah71@gmail.com>
2023-10-29 23:48:45 +0100

    updated

--- packages/app/src/data/project.ts
+++ packages/app/src/data/project.ts
@@ -5,3 +5,36 @@ export const ProjectLabels = [
   "Nebula",
   "Odyssey",
 ];
+
+export const projectTempData = [
+  {
+    name: "Event App",
+    label: "Apex",
+    description: "Source code for my event app",
+    id: 1,
+  },
+  {
+    name: "E-commerce Platform",
+    label: "Vortex",
+    description: "Development of an online marketplace",
+    id: 2,
+  },
+  {
+    name: "Health and Wellness App",
+    label: "Serenity",
+    description: "Creating a fitness and well-being application",
+    id: 3,
+  },
+  {
+    name: "Astronomy Portal",
+    label: "Nebula",
+    description: "Building a website for astronomy enthusiasts",
+    id: 4,
+  },
+  {
+    name: "Travel Blog",
+    label: "Odyssey",
+    description: "Blogging platform for travel enthusiasts",
+    id: 5,
+  },
+];
