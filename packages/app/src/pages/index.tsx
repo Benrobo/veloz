94c80975da16af04993cdf059f800c0d7d9546bc
Benrobo <alumonabenaiah71@gmail.com>
2023-10-29 22:21:19 +0100

    updated

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,5 +1,10 @@
+import Layout from "@/components/Layout";
 import Image from "next/image";
 
 export default function Home() {
-  return <p className="text-white-100 font-ppSB">Welcome</p>;
+  return (
+    <Layout activePage="home">
+      <p className="text-white-100 font-ppSB">Welcome</p>
+    </Layout>
+  );
 }
