5924010112a1f0f9ed67b91425986895da3f4db9
Benrobo <alumonabenaiah71@gmail.com>
2023-10-30 20:20:04 +0100

    updated

--- packages/app/src/components/Flex.tsx
+++ packages/app/src/components/Flex.tsx
@@ -1,21 +1,18 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface FlexProps extends React.HTMLProps<HTMLDivElement> {
   children: ReactNode;
+  className?: React.ComponentProps<"div">["className"];
 }
 
-export function FlexColStart({ children, ...props }: FlexProps) {
-  return (
-    <div className="w-auto flex flex-col items-start justify-start" {...props}>
-      {children}
-    </div>
-  );
-}
-
-export function FlexColCenter({ children, ...props }: FlexProps) {
+export function FlexColStart({ children, className, ...props }: FlexProps) {
   return (
     <div
-      className="w-auto flex flex-col items-center justify-center"
+      className={twMerge(
+        "w-auto flex flex-col items-start justify-start gap-3",
+        className
+      )}
       {...props}
     >
       {children}
@@ -23,33 +20,96 @@ export function FlexColCenter({ children, ...props }: FlexProps) {
   );
 }
 
-export function FlexRowStart({ children, ...props }: FlexProps) {
+export function FlexColEnd({ children, className, ...props }: FlexProps) {
   return (
-    <div className="w-auto flex items-start justify-start" {...props}>
+    <div
+      className={twMerge(
+        "w-auto flex flex-col items-start justify-end gap-3",
+        className
+      )}
+      {...props}
+    >
       {children}
     </div>
   );
 }
 
-export function FlexRowStartBtw({ children, ...props }: FlexProps) {
+export function FlexColCenter({ children, className, ...props }: FlexProps) {
   return (
-    <div className="w-auto flex items-start justify-between" {...props}>
+    <div
+      className={twMerge(
+        "w-auto flex flex-col items-center justify-center gap-3",
+        className
+      )}
+      {...props}
+    >
       {children}
     </div>
   );
 }
 
-export function FlexRowCenter({ children, ...props }: FlexProps) {
+export function FlexRowStart({ children, className, ...props }: FlexProps) {
   return (
-    <div className="w-auto flex items-center justify-center" {...props}>
+    <div
+      className={twMerge(
+        "w-auto flex items-start justify-start gap-3",
+        className
+      )}
+      {...props}
+    >
       {children}
     </div>
   );
 }
 
-export function FlexRowCenterBtw({ children, ...props }: FlexProps) {
+export function FlexRowEnd({ children, className, ...props }: FlexProps) {
   return (
-    <div className="w-auto flex items-center justify-between" {...props}>
+    <div
+      className={twMerge("w-auto flex items-end justify-end gap-3", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function FlexRowStartBtw({ children, className, ...props }: FlexProps) {
+  return (
+    <div
+      className={twMerge(
+        "w-auto flex items-start justify-between gap-3",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function FlexRowCenter({ children, className, ...props }: FlexProps) {
+  return (
+    <div
+      className={twMerge(
+        "w-auto flex items-center justify-center gap-3",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function FlexRowCenterBtw({ children, className, ...props }: FlexProps) {
+  return (
+    <div
+      className={twMerge(
+        "w-auto flex items-center justify-between gap-3",
+        className
+      )}
+      {...props}
+    >
       {children}
     </div>
   );
