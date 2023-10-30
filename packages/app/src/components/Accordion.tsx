71a7226391df60c48b5387c947b61c355af7b79c
Benrobo <alumonabenaiah71@gmail.com>
2023-10-30 20:19:59 +0100

    updated

---
+++ packages/app/src/components/Accordion.tsx
@@ -0,0 +1,75 @@
+import React, { ReactNode, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import {
+  FlexRowCenter,
+  FlexRowCenterBtw,
+  FlexRowStart,
+  FlexRowStartBtw,
+} from "./Flex";
+import { ChevronDown, Layers } from "lucide-react";
+
+interface AccordionProps extends React.HTMLProps<HTMLButtonElement> {
+  children: ReactNode;
+  className?: React.ComponentProps<"div">["className"];
+  title?: string;
+  leftIcon?: React.ReactElement;
+  rightIcon?: React.ReactElement;
+}
+
+function Accordion({
+  children,
+  title,
+  leftIcon,
+  rightIcon,
+  className,
+  ...props
+}: AccordionProps) {
+  const [open, setOpen] = useState(false);
+  return (
+    // @ts-expect-error
+    <button
+      className={twMerge(
+        "w-full h-auto group transition-all outline-none border-solid border-[1px] border-dark-200 px-4 py-4 rounded-lg flex items-start justify-start flex-col",
+        className
+      )}
+      {...props}
+      onClick={() => setOpen(!open)}
+    >
+      <FlexRowStartBtw className="w-full">
+        <FlexRowCenter>
+          {leftIcon ?? (
+            <Layers
+              size={20}
+              className="text-white-300 group-hover:text-white-100"
+            />
+          )}
+          <p className="text-white-200 text-[15px] group-hover:text-white-100 font-ppR">
+            {title ?? "Accordion"}
+          </p>
+        </FlexRowCenter>
+        <FlexRowCenter>
+          {rightIcon ?? (
+            <ChevronDown
+              className={twMerge(
+                "text-white-300 group-hover:text-white-100 transition-all",
+                open
+                  ? "rotate-[-180deg] transition-all"
+                  : "rotage-[180deg] transition-all"
+              )}
+            />
+          )}
+        </FlexRowCenter>
+      </FlexRowStartBtw>
+      <div
+        className={twMerge(
+          "w-full transition-all overflow-hidden",
+          open ? "h-auto mt-5 transition-all" : "h-[0px] transition-all"
+        )}
+      >
+        {children}
+      </div>
+    </button>
+  );
+}
+
+export default Accordion;
