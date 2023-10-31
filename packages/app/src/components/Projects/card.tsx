8205cc26efc72adbf59c07b0dbf3895e69a3b99d
Benrobo <alumonabenaiah71@gmail.com>
2023-10-31 08:22:46 +0100

    updated

--- packages/app/src/components/Projects/card.tsx
+++ packages/app/src/components/Projects/card.tsx
@@ -1,6 +1,6 @@
 import { Box, CloudFog, Convertshape, Crown, Nebulas } from "iconsax-react";
 import React from "react";
-import { twMerge } from "tailwind-merge";
+import ProjectStatus from "./Badge";
 
 export type ProjectType = "Vortex" | "Apex" | "Serenity" | "Nebula" | "Odyssey";
 
@@ -41,33 +41,6 @@ function ProjectCard({
 }
 
 export default ProjectCard;
-
-function ProjectStatus({ status }: { status: "done" | "failed" | "pending" }) {
-  let badge = null;
-  const baseClass = `w-auto px-3 py-1 rounded-[30px] text-[9px] font-ppSB border-solid border-[.5px] border-white-600`;
-  if (status === "pending") {
-    badge = (
-      <span className={twMerge(baseClass, "bg-orange-301 text-orange-300")}>
-        {status}
-      </span>
-    );
-  }
-  if (status === "done") {
-    badge = (
-      <span className={twMerge(baseClass, "bg-green-200 text-green-100")}>
-        {status}
-      </span>
-    );
-  }
-  if (status === "failed") {
-    badge = (
-      <span className={twMerge(baseClass, "bg-red-100 text-red-305")}>
-        {status}
-      </span>
-    );
-  }
-  return badge;
-}
 
 function renderProjectIcons(type: ProjectType) {
   let icon = null;
