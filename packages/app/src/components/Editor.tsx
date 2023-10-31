690b3fa8537a109b4756f2efca5b75cf37588a80
Benrobo <alumonabenaiah71@gmail.com>
2023-10-31 22:08:08 +0100

    updated

--- packages/app/src/components/Editor.tsx
+++ packages/app/src/components/Editor.tsx
@@ -5,10 +5,12 @@ import { twMerge } from "tailwind-merge";
 
 interface EditorProps {
   defaultValue?: string;
-  onChangeText: (event: Event) => void;
+  onChangeText?: (event: Event) => void;
   className?: React.ComponentProps<"div">["className"];
   theme?: "vs-dark" | "vs-light";
   language?: string;
+  readonly?: boolean;
+  lineNumbers?: "off" | "on" | "interval" | "relative";
 }
 
 export default function Editor({
@@ -17,38 +19,26 @@ export default function Editor({
   defaultValue,
   language,
   theme,
+  readonly,
+  lineNumbers,
 }: EditorProps) {
   useEffect(() => {
-    setEditorTheme(monaco);
+    // setEditorTheme(monaco);
   }, []);
   return (
     <MonacoEditor
-      className={twMerge("h-[30vh] bg-dark-200", className)}
+      className={twMerge("h-[40vh] bg-dark-200", className)}
       defaultLanguage={language ?? "typescript"}
-      defaultValue={defaultValue ?? "// some comment"}
-      //   theme={theme ?? "vs-dark"}
+      defaultValue={"\n" + defaultValue ?? "// some comment"}
+      theme={theme ?? "vs-dark"}
       onChange={(e: any) => {
-        onChangeText(e.target.value);
+        onChangeText && onChangeText(e.target.value);
       }}
-      beforeMount={setEditorTheme}
+      options={{
+        readOnly: readonly,
+        domReadOnly: true,
+        lineNumbers,
+      }}
     />
   );
-}
-
-function setEditorTheme(monaco: any) {
-  monaco.editor.defineTheme("onedark", {
-    base: "vs-dark",
-    inherit: true,
-    rules: [
-      {
-        token: "comment",
-        foreground: "#5d7988",
-        fontStyle: "italic",
-      },
-      { token: "constant", foreground: "#e06c75" },
-    ],
-    colors: {
-      "editor.background": "#24202d",
-    },
-  });
 }
