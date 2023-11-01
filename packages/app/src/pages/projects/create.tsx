01d8129672b6d49ac0618745eb8215f314243fb4
Benrobo <alumonabenaiah71@gmail.com>
2023-11-01 18:13:10 +0100

    updated

--- packages/app/src/pages/projects/create.tsx
+++ packages/app/src/pages/projects/create.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import Layout from "@/components/Layout";
 import {
   FlexColCenter,
@@ -8,7 +8,19 @@ import {
   FlexRowStartBtw,
   FlexRowStartCenter,
 } from "@/components/Flex";
-import { ArrowLeftToLine, KeyRound, Layers, PackageCheck } from "lucide-react";
+import {
+  ArrowLeftToLine,
+  DatabaseZap,
+  KeyRound,
+  Layers,
+  LayoutDashboard,
+  Mails,
+  PackageCheck,
+  Paintbrush2,
+  Server,
+  Theater,
+  WalletCards,
+} from "lucide-react";
 import Link from "next/link";
 import { ProjectType, RenderProjectIcons } from "@/components/Projects/Card";
 import { Button } from "@/components/ui/button";
@@ -181,6 +193,7 @@ function CreateProject() {
                   name="codebase_architecture"
                   title="Codebase architecture"
                   className="w-full"
+                  leftIcon={renderAccdIcon("codebase_acrhitecture")}
                 >
                   <FlexRowStart className="mb-3">
                     <p className="text-gray-100 font-ppR text-[12px]">
@@ -199,7 +212,12 @@ function CreateProject() {
                   </FlexRowStartCenter>
                 </Accordion>
                 {/* Frontend Section */}
-                <Accordion name="frontend" title="Frontend" className="w-full">
+                <Accordion
+                  name="frontend"
+                  title="Frontend"
+                  className="w-full"
+                  leftIcon={renderAccdIcon("frontend")}
+                >
                   <FlexRowStartCenter>
                     <RenderSelectableStacks
                       category={"frontend"}
@@ -214,6 +232,7 @@ function CreateProject() {
                   name="design_system"
                   title="Design System"
                   className="w-full"
+                  leftIcon={renderAccdIcon("design_system")}
                 >
                   <FlexRowStartCenter>
                     <RenderSelectableStacks
@@ -225,10 +244,63 @@ function CreateProject() {
                 </Accordion>
 
                 {/* Backend Section */}
-                <Accordion name="backend" title="Backend" className="w-full">
+                <Accordion
+                  name="backend"
+                  title="Backend"
+                  className="w-full"
+                  leftIcon={renderAccdIcon("backend")}
+                >
                   <FlexRowStartCenter>
                     <RenderSelectableStacks
                       category={"backend"}
+                      updateStacksState={updateStacksState}
+                      selecedStacks={selectedStacks}
+                    />
+                  </FlexRowStartCenter>
+                </Accordion>
+
+                {/* Database section */}
+                <Accordion
+                  name="database"
+                  title="Database"
+                  className="w-full"
+                  leftIcon={renderAccdIcon("database")}
+                >
+                  <FlexRowStartCenter>
+                    <RenderSelectableStacks
+                      category={"database"}
+                      updateStacksState={updateStacksState}
+                      selecedStacks={selectedStacks}
+                    />
+                  </FlexRowStartCenter>
+                </Accordion>
+
+                {/* Payment Section */}
+                <Accordion
+                  name="payment"
+                  title="Payment"
+                  className="w-full"
+                  leftIcon={renderAccdIcon("payment")}
+                >
+                  <FlexRowStartCenter>
+                    <RenderSelectableStacks
+                      category={"payment"}
+                      updateStacksState={updateStacksState}
+                      selecedStacks={selectedStacks}
+                    />
+                  </FlexRowStartCenter>
+                </Accordion>
+
+                {/* Mailing */}
+                <Accordion
+                  name="mailing"
+                  title="Mailing"
+                  className="w-full"
+                  leftIcon={renderAccdIcon("mailing")}
+                >
+                  <FlexRowStartCenter>
+                    <RenderSelectableStacks
+                      category={"mailing"}
                       updateStacksState={updateStacksState}
                       selecedStacks={selectedStacks}
                     />
@@ -282,4 +354,39 @@ function renderSidebarConfigIcon(key: ProjectSideBarConfigKeysType) {
     icon = <Layers className="group-hover:text-white-100" />;
   }
   return icon;
+}
+
+function renderAccdIcon(category: TechStackCategory) {
+  let icon = null;
+  if (category === "codebase_acrhitecture") {
+    icon = (
+      <LayoutDashboard className="group-hover:text-white-100 text-white-300" />
+    );
+  }
+  if (category === "frontend") {
+    icon = <Theater className="group-hover:text-white-100 text-white-300" />;
+  }
+  if (category === "design_system") {
+    icon = (
+      <Paintbrush2 className="group-hover:text-white-100 text-white-300" />
+    );
+  }
+  if (category === "backend") {
+    icon = <Server className="group-hover:text-white-100 text-white-300" />;
+  }
+  if (category === "database") {
+    icon = (
+      <DatabaseZap className="group-hover:text-white-100 text-white-300" />
+    );
+  }
+  if (category === "payment") {
+    icon = (
+      <WalletCards className="group-hover:text-white-100 text-white-300" />
+    );
+  }
+  if (category === "mailing") {
+    icon = <Mails className="group-hover:text-white-100 text-white-300" />;
+  }
+
+  return icon as ReactElement;
 }
