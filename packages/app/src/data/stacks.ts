7254e2dffe9b63a8111f23099586b53b7c0684eb
Benrobo <alumonabenaiah71@gmail.com>
2023-11-01 10:57:54 +0100

    updated

--- packages/app/src/data/stacks.ts
+++ packages/app/src/data/stacks.ts
@@ -1,46 +1,102 @@
-const STACKS_LOGOS = [
+import { TechStacks } from "../../types";
+
+const TECH_STACKS = [
+  // Codebase Architecture
+  {
+    name: "Monolith",
+    key: "monolith",
+    img: "/images/cb_architecture/monolith.png",
+    pricing_plan: "BASIC_PKG",
+    category: "codebase_acrhitecture",
+    available: true,
+    supported_architecture: ["monolith"],
+  },
+  {
+    name: "Monorepo",
+    key: "monorepo",
+    img: "/images/cb_architecture/monorepo.png",
+    pricing_plan: "STANDARD_PKG",
+    category: "codebase_acrhitecture",
+    available: true,
+    supported_architecture: ["monorepo"],
+  },
+  // Frontend
   {
     name: "nextjs",
     key: "nextjs",
     img: "/images/tech_stacks/nextjs.svg",
+    pricing_plan: "BASIC_PKG",
+    category: "frontend",
+    available: true,
+    supported_architecture: ["monolith", "monorepo"],
   },
+  // Design System
   {
     name: "tailwindcss",
     key: "tailwindcss",
     img: "/images/tech_stacks/tailwindcss.svg",
+    pricing_plan: "BASIC_PKG",
+    category: "design_system",
+    available: true,
+    supported_architecture: ["monolith"],
   },
+  // BACKEND
   {
     name: "nodejs",
     key: "nodejs",
     img: "/images/tech_stacks/nodejs.svg",
+    pricing_plan: "PRO_PKG",
+    category: "backend",
+    available: true,
+    supported_architecture: ["monolith"],
   },
   {
     name: "mysql",
     key: "mysql",
     img: "/images/tech_stacks/mysql.svg",
+    pricing_plan: "BASIC_PKG",
+    category: "backend",
+    available: true,
+    supported_architecture: ["monolith"],
   },
   //   Payment Services
   {
     name: "Lemonsqueezy",
     key: "lemonsqueezy",
     img: "/images/payment/lemonsqueezy.svg",
+    pricing_plan: "BASIC_PKG",
+    category: "payment",
+    available: true,
+    supported_architecture: ["monolith"],
   },
   {
     name: "Stripe",
     key: "stripe",
     img: "/images/payment/stripe.svg",
+    pricing_plan: "BASIC_PKG",
+    category: "payment",
+    available: false,
+    supported_architecture: ["monolith"],
   },
   //   Email Services
   {
     name: "Sendgrid",
     key: "sendgrid",
     img: "/images/mail/sendgrid.png",
+    pricing_plan: "BASIC_PKG",
+    category: "mailing",
+    available: true,
+    supported_architecture: ["monolith"],
   },
   {
     name: "Resend",
     key: "resend",
     img: "/images/mail/resend.png",
+    pricing_plan: "BASIC_PKG",
+    category: "mailing",
+    available: true,
+    supported_architecture: ["monolith"],
   },
-];
+] satisfies TechStacks[];
 
-export default STACKS_LOGOS;
+export default TECH_STACKS;
