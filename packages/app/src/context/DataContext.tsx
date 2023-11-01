07cf115f839b56433fe6bb333771b55b871bcae0
Benrobo <alumonabenaiah71@gmail.com>
2023-11-01 17:36:37 +0100

    updated

--- packages/app/src/context/DataContext.tsx
+++ packages/app/src/context/DataContext.tsx
@@ -1,8 +1,12 @@
 import React, { ReactNode, createContext, useContext, useState } from "react";
 import { TechStackPricingPlan } from "../../types";
+import FreemiumModal from "@/components/FreemiumModal";
 
 interface ContextValuesType {
   userPlan: TechStackPricingPlan;
+  pkgPlan: TechStackPricingPlan;
+  setPkgPlan: (plan: TechStackPricingPlan) => void;
+  togglePremiumModalVisibility: () => void;
 }
 
 export const DataContext = createContext<ContextValuesType>(
@@ -11,12 +15,25 @@ export const DataContext = createContext<ContextValuesType>(
 
 function DataContextProvider({ children }: { children: ReactNode }) {
   const [userPlan, setUserPlan] = useState<TechStackPricingPlan>("BASIC_PKG");
+  const [premiumModal, setPremiumModal] = useState(false);
+  const [pkgPlan, setPkgPlan] = useState<TechStackPricingPlan>("BASIC_PKG");
+  const togglePremiumModalVisibility = () => setPremiumModal(!premiumModal);
+
   const contextValues: ContextValuesType = {
     userPlan,
+    pkgPlan,
+    setPkgPlan,
+    togglePremiumModalVisibility,
   };
+
   return (
     <DataContext.Provider value={contextValues}>
       {children}
+      <FreemiumModal
+        onClose={togglePremiumModalVisibility}
+        isOpen={premiumModal}
+        price_plan={pkgPlan}
+      />
     </DataContext.Provider>
   );
 }
