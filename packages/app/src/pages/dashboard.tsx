4558bdabdf76a44d26506614ed21a088d2a6c897
Benrobo <alumonabenaiah71@gmail.com>
2023-11-03 18:30:47 +0100

    updated

--- packages/app/src/pages/dashboard.tsx
+++ packages/app/src/pages/dashboard.tsx
@@ -1,10 +1,17 @@
+import { FlexColCenter } from "@/components/Flex";
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout activePage="home">
-      <p className="text-white-100 font-ppSB">Welcome</p>
+      <p className="text-white-100">Dashboard</p>
+      <Modal isBlurBg isOpen>
+        <FlexColCenter className="w-full h-full">
+          <h1 className="text-white-100 text-3xl font-ppEB">Coming Soon</h1>
+        </FlexColCenter>
+      </Modal>
     </Layout>
   );
 }
