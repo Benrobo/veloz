c5a78397847196ca22918c5304e521e4e4a2888f
Benrobo <alumonabenaiah71@gmail.com>
2023-11-04 00:53:36 +0100

    updated

--- packages/app/src/data/images.ts
+++ packages/app/src/data/images.ts
@@ -3,3 +3,73 @@ const LocalImages = {
 };
 
 export default LocalImages;
+
+export const StackImages = [
+  {
+    name: "nextjs",
+    img: "/images/tech_stacks/javascript.svg",
+  },
+  {
+    name: "nodejs",
+    img: "/images/tech_stacks/nodejs.svg",
+  },
+  {
+    name: "golang",
+    img: "/images/tech_stacks/golang.svg",
+  },
+  {
+    name: "javascript",
+    img: "/images/tech_stacks/javascript.svg",
+  },
+  {
+    name: "mongodb",
+    img: "/images/tech_stacks/mongodb.svg",
+  },
+  {
+    name: "nextjs-api",
+    img: "/images/tech_stacks/nextjs-api.png",
+  },
+  {
+    name: "mysql",
+    img: "/images/tech_stacks/mysql.svg",
+  },
+  {
+    name: "tailwindcss",
+    img: "/images/tech_stacks/tailwindcss.svg",
+  },
+  {
+    name: "react",
+    img: "/images/tech_stacks/react.svg",
+  },
+  {
+    name: "postgres",
+    img: "/images/tech_stacks/postgres.svg",
+  },
+];
+
+export const FineTunedStacksImg = [
+  {
+    name: "zeus",
+    img: "/images/finetuned/zeus.jpeg",
+  },
+  {
+    name: "hera",
+    img: "/images/finetuned/hera.jpeg",
+  },
+  {
+    name: "athena",
+    img: "/images/finetuned/athena.jpeg",
+  },
+  {
+    name: "poseidon",
+    img: "/images/finetuned/poseidon.jpeg",
+  },
+  {
+    name: "ares",
+    img: "/images/finetuned/ares.jpeg",
+  },
+  {
+    name: "dynamo",
+    img: "/images/finetuned/dynamo.jpeg",
+  },
+];
