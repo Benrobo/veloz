c5fec8268eee74eb03b7790ce1e0d6612edbf4f4
Benrobo <alumonabenaiah71@gmail.com>
2023-11-08 16:10:30 +0100

    updated

---
+++ packages/app/src/pages/api/lib/utils.ts
@@ -0,0 +1,10 @@
+import { connect } from "mongoose";
+import env from "../config/env";
+
+export async function connectDB() {
+  try {
+    await connect(env.MONGO_DB_URL as string);
+  } catch (e: any) {
+    console.log(`Error connecting to mongodb: ${e?.message}`);
+  }
+}
