60418edc687a8060009144e58368b9adc43f1ea1
Benrobo <alumonabenaiah71@gmail.com>
2023-11-08 18:59:03 +0100

    updated

--- packages/app/src/pages/api/config/env.ts
+++ packages/app/src/pages/api/config/env.ts
@@ -3,6 +3,7 @@ const env = {
     process.env.NODE_ENV === "development"
       ? process.env.MONGODB_LOCAL_URL
       : process.env.MONGODB_PROD_URL,
+  CLERK_WH_SECRET: process.env.CLERK_WH_SECRET,
 };
 
 export default env;
