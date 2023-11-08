174df9776693cbe0530b9ee3c593bb2a196a68de
Benrobo <alumonabenaiah71@gmail.com>
2023-11-08 16:30:41 +0100

    added models

---
+++ packages/app/src/pages/api/models/secrets.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import { connectDB } from "../lib/utils";
+import { VelozProjectType } from "@veloz/shared/types";
+import { TechPricingPlans } from "@veloz/shared/data/project";
+
+// 1. Create an interface representing a document in MongoDB.
+interface ISecrets {
+  name: string;
+  category: VelozProjectType;
+  secrets: {
+    id: any;
+    name: string;
+    value: string;
+  }[];
+  createdAt?: Date;
+}
+
+const secretsSchema = new Schema({
+  name: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const secretSchema = new Schema<ISecrets>(
+  {
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    secrets: [secretsSchema],
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const Secret = model<ISecrets>("Secret", secretSchema);
+
+export default Secret;
+
+// connect to db
+connectDB();
