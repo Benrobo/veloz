f68ffbc3de255cae5bdb7cd5c8993bbaaad93f68
Benrobo <alumonabenaiah71@gmail.com>
2023-11-10 20:50:51 +0100

    updated

---
+++ packages/app/src/pages/api/services/secret.services.ts
@@ -0,0 +1,70 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Secret } from "../models";
+import sendResponse from "../lib/sendResponse";
+import { RESPONSE_CODE } from "@veloz/shared";
+import nextRouteZodValidation from "../lib/nextRouteZodValidation";
+import { createSecretSchema } from "../lib/validationSchema";
+import { createSecretSchemaType } from "@/types";
+
+class SecretService {
+  async create(req: NextApiRequest, res: NextApiResponse) {
+    const userId = (req as any)?.user?.id;
+    const payload = req.body as createSecretSchemaType;
+    await nextRouteZodValidation(createSecretSchema, req, res);
+
+    // check if secret name exists
+    const secret = await Secret.findOne({
+      uId: userId,
+      name: payload?.name,
+      category: payload?.category,
+    });
+
+    if (secret) {
+      return sendResponse.error(
+        res,
+        RESPONSE_CODE.SECRET_EXISTS,
+        `Secret already exists`,
+        400
+      );
+    }
+
+    // sanitize secrets data & create secrets
+    const secrets = payload?.secrets?.map((secret) => {
+      const key = secret?.name?.replace(/\s+/g, "_").toUpperCase();
+      const value = secret?.value?.replace(/\s+/g, "_").toLowerCase();
+      return {
+        name: key,
+        value,
+      };
+    });
+
+    await Secret.create({
+      uId: userId,
+      name: payload?.name,
+      category: payload?.category,
+      secrets: secrets,
+    });
+
+    sendResponse.success(
+      res,
+      RESPONSE_CODE.SECRET_CREATED,
+      `Secret created`,
+      200
+    );
+  }
+
+  async getAll(req: NextApiRequest, res: NextApiResponse) {
+    const userId = (req as any)?.user?.id;
+    const secrets = await Secret.find({ uId: userId });
+
+    sendResponse.success(
+      res,
+      RESPONSE_CODE.SECRET_DETAILS,
+      `Secret details`,
+      200,
+      secrets
+    );
+  }
+}
+
+export default new SecretService();
