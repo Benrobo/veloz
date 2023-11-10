83b32ce087089511e8ef1d896c2169e6fc8938cd
Benrobo <alumonabenaiah71@gmail.com>
2023-11-10 19:32:48 +0100

    updated

--- packages/app/src/context/DataContext.tsx
+++ packages/app/src/context/DataContext.tsx
@@ -1,12 +1,18 @@
 import React, { ReactNode, createContext, useContext, useState } from "react";
 import { ProjectType, TechStackPricingPlan } from "@veloz/shared/types";
 import FreemiumModal from "@/components/FreemiumModal";
+import { UserInfo } from "@/types";
 
 interface ContextValuesType {
   userPlan: TechStackPricingPlan;
+  setUserPlan: React.Dispatch<React.SetStateAction<TechStackPricingPlan>>;
   pkgPlan: TechStackPricingPlan;
-  setPkgPlan: (plan: TechStackPricingPlan) => void;
+  setPkgPlan: React.Dispatch<React.SetStateAction<TechStackPricingPlan>>;
   togglePremiumModalVisibility: () => void;
+  userInfo: UserInfo;
+  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo>>;
+  globalLoading: boolean;
+  setGlobalLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const DataContext = createContext<ContextValuesType>(
@@ -14,18 +20,24 @@ export const DataContext = createContext<ContextValuesType>(
 );
 
 function DataContextProvider({ children }: { children: ReactNode }) {
-  const [userPlan, setUserPlan] =
-    useState<TechStackPricingPlan>("ENTERPRISE_PKG");
+  const [userPlan, setUserPlan] = useState<TechStackPricingPlan>("FREE_PKG");
   const [premiumModal, setPremiumModal] = useState(false);
   const [pkgPlan, setPkgPlan] =
     useState<TechStackPricingPlan>("ENTERPRISE_PKG");
   const togglePremiumModalVisibility = () => setPremiumModal(!premiumModal);
+  const [userInfo, setUserInfo] = useState<UserInfo>({} as UserInfo);
+  const [globalLoading, setGlobalLoading] = useState(false);
 
   const contextValues: ContextValuesType = {
     userPlan,
+    setUserPlan,
     pkgPlan,
     setPkgPlan,
     togglePremiumModalVisibility,
+    userInfo,
+    setUserInfo,
+    globalLoading,
+    setGlobalLoading,
   };
 
   return (
