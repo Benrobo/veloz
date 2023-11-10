04498269fcd41eabea8b1fb12e714c190b9e5672
Benrobo <alumonabenaiah71@gmail.com>
2023-11-10 20:50:41 +0100

    updated

--- packages/app/src/lib/http/requests.ts
+++ packages/app/src/lib/http/requests.ts
@@ -4,3 +4,8 @@ export const getUser = async () => {
   const req = await $axios.get("/api/user");
   return req.data;
 };
+
+export const createSecret = async (payload: any) => {
+  const req = await $axios.post("/api/secret", payload);
+  return req.data;
+};
