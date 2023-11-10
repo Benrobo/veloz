1c1fcefb63f36aeea8378aa0df5e8d3cf99860ce
Benrobo <alumonabenaiah71@gmail.com>
2023-11-10 20:50:48 +0100

    updated

---
+++ packages/app/src/pages/api/secret/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import CatchError from "../lib/error";
+import { isAuthenticated } from "../middlewares/auth";
+import secretServices from "../services/secret.services";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    await secretServices.create(req, res);
+  }
+  if (req.method === "GET") {
+    await secretServices.getAll(req, res);
+  }
+}
+
+export default CatchError(isAuthenticated(handler));
