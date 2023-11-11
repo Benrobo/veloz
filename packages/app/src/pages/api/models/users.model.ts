3f63a051f2077c6e4e00a48b5411093ebbd86fe8
Benrobo <alumonabenaiah71@gmail.com>
2023-11-11 19:56:19 +0100

    updated user model

--- packages/app/src/pages/api/models/users.model.ts
+++ packages/app/src/pages/api/models/users.model.ts
@@ -13,8 +13,21 @@ interface IUser {
   proj_plan: TechStackPricingPlan;
   hasSubscribed: boolean;
   isTester: boolean;
+  github_token: string;
+  project_config: {
+    default_nextjs_router: "APP" | "PAGE";
+  };
   createdAt?: Date;
 }
+
+enum NextjsRouter {
+  APP = "APP",
+  PAGE = "PAGE",
+}
+
+const projectConfigSchema = new Schema({
+  default_nextjs_router: { type: NextjsRouter, required: true, default: "APP" },
+});
 
 const userSchema = new Schema<IUser>(
   {
@@ -26,6 +39,8 @@ const userSchema = new Schema<IUser>(
     role: { type: String, enum: ["user", "admin"], default: "user" },
     hasSubscribed: { type: Boolean, default: false },
     isTester: { type: Boolean, default: false },
+    github_token: { type: String, default: null },
+    project_config: projectConfigSchema,
     createdAt: { type: Date, default: Date.now },
   },
   {
