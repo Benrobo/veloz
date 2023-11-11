0eb626e580125d0443d3e8abd3134fd761323b64
Benrobo <alumonabenaiah71@gmail.com>
2023-11-11 19:16:56 +0100

    updated

--- packages/app/src/pages/api/lib/validationSchema.ts
+++ packages/app/src/pages/api/lib/validationSchema.ts
@@ -1,29 +1,46 @@
 import { z as zod } from "zod";
 
 // validate creating secrets
-enum CategoryEnum {
-  frontend = "frontend",
-  backend = "backend",
-}
+
 export const createSecretSchema = zod.object({
   name: zod.string(),
-  category: zod.nativeEnum(CategoryEnum, {
-    errorMap: (issue, _ctx) => {
-      switch (issue.code) {
-        case "invalid_type":
-          return { message: "Invalid category" };
-        case "invalid_enum_value":
-          return { message: "Invalid category" };
-        default:
-          return { message: "Invalid category" };
-      }
-    },
-  }),
   secrets: zod.array(
     zod.object({
       // id: zod.any(),
       name: zod.string(),
       value: zod.string(),
     })
+  ),
+});
+
+export const updateSecretSchema = zod.object({
+  id: zod.string(),
+  name: zod.string(),
+  deleteEnv: zod.array(
+    zod
+      .object({
+        id: zod.any(),
+        name: zod.string(),
+        value: zod.string(),
+      })
+      .optional()
+  ),
+  updateEnv: zod.array(
+    zod
+      .object({
+        id: zod.any(),
+        name: zod.string(),
+        value: zod.string(),
+      })
+      .optional()
+  ),
+  createEnv: zod.array(
+    zod
+      .object({
+        id: zod.any(),
+        name: zod.string(),
+        value: zod.string(),
+      })
+      .optional()
   ),
 });
