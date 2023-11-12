1ed70557827a8022781ef1c31404a52850d856d7
Benrobo <alumonabenaiah71@gmail.com>
2023-11-12 16:25:30 +0100

    update

--- packages/app/src/components/Stacks/Render.tsx
+++ packages/app/src/components/Stacks/Render.tsx
@@ -201,6 +201,68 @@ export function RenderSelectableStacks({
   );
 }
 
+export function RenderFineTunedStacks({
+  tech_stacks,
+}: {
+  tech_stacks: string[];
+}) {
+  const validStacks: StackObj[] = [];
+  const stacks = REFINED_STACKS_DATA.map((stk: any) => stk.stacks);
+  const _traversedStacks: { name: string; key: string }[] = [];
+  const stackData: string[] = [];
+
+  if (tech_stacks.length > 0) {
+    for (const st of stacks) {
+      for (const s of st) {
+        stackData.push(s.key);
+        _traversedStacks.push({ name: s.name, key: s.key });
+      }
+    }
+    for (const d of tech_stacks) {
+      if (d.length > 0) {
+        if (stackData?.includes(d)) {
+          const foundStack = _traversedStacks?.find((stack) => stack.key === d);
+          validStacks.push({
+            name: foundStack?.name as string,
+            key: d,
+            img: getImageUrl(d) as string,
+          });
+        }
+      }
+    }
+  }
+
+  return (
+    <FlexRowStart className="w-full flex-wrap gap-8">
+      {validStacks.map((stack) => (
+        <FlexColCenter key={stack.key} className="w-fit h-[70px]">
+          <Image
+            width={
+              stackWithExtendedHeight.includes(stack.key)
+                ? 50
+                : stack.key === "lemonsqueezy"
+                ? 120
+                : 30
+            }
+            height={0}
+            alt={stack.key}
+            src={stack.img}
+            className={twMerge(
+              stackWithRoundedCorners.includes(stack.key)
+                ? "rounded-[50%]"
+                : "",
+              stackWithWhiteBg.includes(stack.key) ? "bg-white-105" : ""
+            )}
+          />
+          <span className="px-2 py-1 text-[10px] text-white-100 rounded-md bg-dark-200 font-ppR">
+            {stack.name}
+          </span>
+        </FlexColCenter>
+      ))}
+    </FlexRowStart>
+  );
+}
+
 function getImageUrl(stackName: string) {
   const stack = StackImages.find((s) => s.name === stackName);
   return stack?.img || "";
