d4ce77be4ae8956ac56b67f0cdf1c7d6f6fa2f97
Benrobo <alumonabenaiah71@gmail.com>
2023-11-12 16:06:54 +0100

    update

--- packages/app/src/pages/api/lib/validationSchema.ts
+++ packages/app/src/pages/api/lib/validationSchema.ts
@@ -1,3 +1,6 @@
+import { TechStackCategory } from "@veloz/shared";
+import { ProjectLabels } from "@veloz/shared/data/project";
+import { STACK_CATEGORIES } from "@veloz/shared/data/stack";
 import { z as zod } from "zod";
 
 // validate creating secrets
@@ -42,4 +45,34 @@ export const updateSecretSchema = zod.object({
       })
     )
     .optional(),
+});
+
+// create project validator
+const categorySchema = zod.enum(STACK_CATEGORIES as any);
+
+const stackInfoSchema = zod
+  .object({
+    stack: zod.string(),
+    name: zod.string(),
+    category: categorySchema,
+  })
+  .optional();
+
+export const createProjectSchema = zod.object({
+  name: zod.string(),
+  description: zod.string(),
+  projectOption: zod.enum(["Refined", "Fine-Tuned"]),
+  label: zod.enum(ProjectLabels as any).optional(),
+  env_id: zod.string().optional(),
+  fineTunedStackName: zod.string().optional(),
+  tech_stacks: zod.object({
+    frontend: stackInfoSchema,
+    codebase_acrhitecture: stackInfoSchema,
+    design_system: stackInfoSchema,
+    backend: stackInfoSchema,
+    database: stackInfoSchema,
+    payment: stackInfoSchema,
+    mailing: stackInfoSchema,
+    authentication: stackInfoSchema,
+  }),
 });
