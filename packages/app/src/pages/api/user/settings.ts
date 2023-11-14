020b763e98678786422f94d99e95677317733fc1
Benrobo <alumonabenaiah71@gmail.com>
2023-11-14 11:52:44 +0100

    update

---
+++ packages/app/src/pages/api/user/settings.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import CatchError from "../lib/error";
+import { isAuthenticated } from "../middlewares/auth";
+import userServices from "../services/user.services";
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    return userServices.getSettings(req, res);
+  }
+}
+
+export default CatchError(isAuthenticated(handler));
