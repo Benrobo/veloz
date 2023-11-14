ba46102aed6579f9c312c4030253b8ede326f1b1
Benrobo <alumonabenaiah71@gmail.com>
2023-11-14 20:36:21 +0100

    trying to setup module alias :(

--- packages/sh_api/src/routes.ts
+++ packages/sh_api/src/routes.ts
@@ -1,9 +1,11 @@
+import "module-alias/register";
 import express, { Request, Response } from "express";
-import generateProjectService from "services/generateProject";
+import generateProject from "@services/generateProject";
+// import generateProjectService from "@services/";
 const router = express.Router();
 
 router.get("/test", async (req: Request, res: Response) => {
-  await generateProjectService.test(req, res);
+  await generateProject.test(req, res);
 });
 
 export default router;
--- packages/sh_api/src/services/generateProject.ts
+++ packages/sh_api/src/services/generateProject.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-
+import { User, Project } from "@veloz/shared/models";
 class GenerateProject {
   async test(req: Request, res: Response) {
     res.status(200).json("welcome");
