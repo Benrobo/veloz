ed0cc573cf9eac0b817d280486766436a7188923
Benrobo <alumonabenaiah71@gmail.com>
2023-11-14 02:27:10 +0100

    update

--- packages/app/src/pages/projects/index.tsx
+++ packages/app/src/pages/projects/index.tsx
@@ -2,16 +2,37 @@ import Layout from "@/components/Layout";
 import ProjectCard from "@/components/Projects/Card";
 import Slidebar from "@/components/Projects/Slidebar";
 import { Button } from "@/components/ui/button";
-import { projectTempData } from "@/data/project";
 import { withAuth } from "@/lib/helpers";
 import { Flash } from "iconsax-react";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ProjectType } from "@veloz/shared/types";
+import { ProjectListType } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+import { getProjects } from "@/lib/http/requests";
+import { FlexColStart } from "@/components/Flex";
 
 function Projects() {
+  const [projects, setProjects] = useState<ProjectListType[]>([]);
   const [slideBarOpen, setSlideBarOpen] = useState(false);
   const [selectedProjId, setSelectedProjId] = useState<any>("");
+  const [selectedProject, setSelectedProject] =
+    useState<ProjectListType | null>(null);
+  const getProjectsQuery = useQuery({
+    queryFn: async () => getProjects(),
+    queryKey: ["getProjects"],
+  });
+
+  useEffect(() => {
+    if (getProjectsQuery.data) {
+      const data = getProjectsQuery.data.data as ProjectListType[];
+      setProjects(data);
+    }
+  }, [
+    getProjectsQuery.data,
+    getProjectsQuery.error,
+    getProjectsQuery.isLoading,
+  ]);
 
   const toggleSlideBar = () => setSlideBarOpen(!slideBarOpen);
 
@@ -30,27 +51,40 @@ function Projects() {
       </div>
       <br />
       <div className="w-full px-3 py-3 flex flex-wrap items-center justify-start gap-3">
-        {projectTempData.map((d) => (
-          <ProjectCard
-            key={d.id}
-            name={d.name}
-            description={d.description}
-            status={d.status}
-            label={d.label as ProjectType}
-            id={d.id}
-            onClick={() => {
-              setSelectedProjId(d.id);
-              setSlideBarOpen(true);
-            }}
-          />
-        ))}
+        {projects.length > 0 ? (
+          projects.map((d) => (
+            <ProjectCard
+              key={d._id}
+              name={d.name}
+              description={d.description}
+              status={d.status}
+              label={d.label as ProjectType}
+              id={d._id}
+              onClick={() => {
+                const proj = projects.find(
+                  (p) => p._id === d._id
+                ) as ProjectListType;
+                setSelectedProject(proj);
+                setSelectedProjId(d._id);
+                setSlideBarOpen(true);
+              }}
+            />
+          ))
+        ) : (
+          <FlexColStart className="w-full h-full flex items-center justify-center">
+            <p className="text-white-200 text-[14px] font-jbSB ">No Projects</p>
+            <p className="text-white-200 font-jbR text-[12px] ">
+              Start by creating your first veloz project.
+            </p>
+          </FlexColStart>
+        )}
       </div>
 
       {/* */}
       <Slidebar
         onClose={toggleSlideBar}
         proj_id={selectedProjId}
-        projects={projectTempData}
+        selectedProject={selectedProject as ProjectListType}
         isOpen={slideBarOpen}
       />
     </Layout>
