f5e1a6a545ff03332dbb2af11893cb9405d629a9
Benrobo <alumonabenaiah71@gmail.com>
2023-11-14 01:14:04 +0100

    update

--- packages/shared/utils/index.ts
+++ packages/shared/utils/index.ts
@@ -1,5 +1,5 @@
-import { STACK_AVAILABILITY } from "../data/stack";
-import { TechStackCategory } from "../types";
+import { REFINED_STACKS, STACK_AVAILABILITY } from "../data/stack";
+import { TechStackCategory, TechStackPricingPlan } from "../types";
 
 // check stack availability
 export function isStackAvailable(stack: string, category: TechStackCategory) {
@@ -26,4 +26,28 @@ export function _checkRefinedStackAvailability(stacks: StackProps) {
     }
   }
   return true;
+}
+
+// check if user is eligible or not for a specific stack plan
+export function _isUserEligibleForStack(
+  stacks: StackProps,
+  userPlan: TechStackPricingPlan
+) {
+  let resp = {
+    eligible: false,
+    message: "",
+  };
+  for (const [category, stack] of Object.entries(stacks)) {
+    const _stack = REFINED_STACKS.find(
+      (s) => s.category === category
+    )?.stacks.find((s) => s.key === stack.stack);
+
+    if (_stack?.key === stack.stack && _stack.pricing_plan !== userPlan) {
+      console.log(`[${stack.stack} : ${_stack.pricing_plan}] Not Eligible`);
+      resp.message = `You are not eligible for ${stack.stack} stack`;
+      return resp;
+    }
+  }
+  resp.eligible = true;
+  return resp;
 }
