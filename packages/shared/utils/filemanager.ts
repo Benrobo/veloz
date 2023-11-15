44263b5a38fb19a8b1e5e15b337f4c6a135fcbd8
Benrobo <alumonabenaiah71@gmail.com>
2023-11-16 00:06:26 +0100

    updated

--- packages/shared/utils/filemanager.ts
+++ packages/shared/utils/filemanager.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-export function createDir(path: string, name: string) {
+export function createDir(path: string, name: string, forceDelete?: boolean) {
   const dir = `${path}/${name}`;
   let resp: { success: boolean; msg: string | null; path: string } = {
     success: false,
@@ -18,14 +18,87 @@ export function createDir(path: string, name: string) {
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
+      resp["success"] = true;
+      resp["msg"] = null;
+    } else {
+      if (forceDelete && fs.existsSync(dir)) {
+        removeDirRf(dir, (err, success) => {
+          if (err) {
+            resp["msg"] = err;
+            resp["success"] = false;
+          }
+          if (success) {
+            fs.mkdirSync(dir);
+            resp["msg"] = null;
+            resp["success"] = true;
+          }
+        });
+      }
+    }
+
+    return resp;
+  } catch (e: any) {
+    msg = `Something went wrong creating ${name} directory: ${e?.message}.`;
+    resp["msg"] = msg;
+    resp["success"] = false;
+    return resp;
+  }
+}
+
+export function createFile(
+  path: string,
+  name: string,
+  content?: string,
+  forceDelete?: boolean
+) {
+  const dir = `${path}/${name}`;
+  let resp: { success: boolean; msg: string | null; path: string } = {
+    success: false,
+    msg: null,
+    path: dir,
+  };
+  let msg = "";
+  try {
+    // check if path exists
+    if (!fs.existsSync(path)) {
+      msg = `Failed to create '${name}' file, path don't exist.`;
+      resp["msg"] = msg;
+      return resp;
+    }
+
+    if (!fs.existsSync(dir) && !forceDelete) {
+      fs.writeFileSync(dir, content ?? "");
+    } else {
+      if (forceDelete && fs.existsSync(dir)) {
+        removeDirRf(dir, () => {
+          fs.writeFileSync(dir, content ?? "");
+        });
+      }
     }
 
     resp["success"] = true;
     resp["msg"] = null;
     return resp;
   } catch (e: any) {
-    msg = `Something went wrong creating ${name} directory: ${e?.message}.`;
+    msg = `Something went wrong creating '${name}' file: ${e?.message}.`;
     resp["msg"] = msg;
     return resp;
   }
+}
+
+export function removeDirRf(
+  dir: string,
+  cb: (error?: string | null, success?: boolean) => void
+) {
+  try {
+    if (!fs.existsSync(dir)) {
+      console.log(
+        `[Failed to delete directory recursively]: directory not found`
+      );
+      return;
+    }
+
+    console.log(fs.readdirSync(dir));
+    cb("something went wrong", false);
+  } catch (e: any) {}
 }
