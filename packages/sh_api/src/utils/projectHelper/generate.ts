da7eb2804e569745923249c93154ea6644f37222
Benrobo <alumonabenaiah71@gmail.com>
2023-11-15 12:49:21 +0100

    updated

--- packages/sh_api/src/utils/projectHelper/generate.ts
+++ packages/sh_api/src/utils/projectHelper/generate.ts
@@ -3,18 +3,33 @@ import { REFINED_STACK_VALUE, TechStackCategory } from "@veloz/shared";
 type Props = {
   category: TechStackCategory;
   name: string;
-  stack: string;
+  technology: string;
+  key: string;
   _id: object;
 }[];
 
-export default async function ProjectGenerate(tech_stacks: Props) {
-  for (const _stacks of tech_stacks) {
-    const codebaseArchitecture = _stacks.name;
+export default async function ProjectGenerate(
+  tech_stacks: Props,
+  name: string
+) {
+  const formatedName = name.toLowerCase().replace(/\s/gi, "-");
+  type Arch = "monolith" | "monorepo";
+  const codebaseArchitecture =
+    (tech_stacks.find((s) => s.key === "monorepo")?.key as Arch) ??
+    ("monolith" as Arch);
+  const _frontend = findStack(tech_stacks, "frontend");
+  const _backend = findStack(tech_stacks, "backend");
 
+  for (const _stacks of tech_stacks) {
     // monorepo setup
-    if (codebaseArchitecture?.toLowerCase() === "monorepo") {
+    if (codebaseArchitecture === "monorepo") {
+      console.log(formatedName, _stacks);
     }
-    if (codebaseArchitecture?.toLowerCase() === "monolith") {
+    if (codebaseArchitecture === "monolith") {
     }
   }
+}
+
+function findStack(stacks: Props, key: TechStackCategory) {
+  return stacks.find((s) => s.key === key) ?? null;
 }
