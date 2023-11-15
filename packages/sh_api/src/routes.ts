991c46cd1c7961f9f0b50637814456380417e73a
Benrobo <alumonabenaiah71@gmail.com>
2023-11-16 00:04:10 +0100

    updated

--- packages/sh_api/src/routes.ts
+++ packages/sh_api/src/routes.ts
@@ -7,11 +7,13 @@ import { ParamsDictionary } from "express-serve-static-core";
 import { CatchError } from "./utils/error";
 import { connectDB } from "@veloz/shared/utils/mongodb";
 import sh_env from "./config/env";
+import GenerateProject from "@/services/generateProject";
 
 const router = express.Router();
 
 connectDB(sh_env.MONGO_DB_URL as string);
 
 router.post("/project/generate", CatchError(generateProject.generate));
+// router.post("/project/generate", CatchError(new GenerateProject().generate));
 
 export default router;
