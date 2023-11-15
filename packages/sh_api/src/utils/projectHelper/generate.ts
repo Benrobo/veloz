8b59b333424ef076702b766b765dc394d760561f
Benrobo <alumonabenaiah71@gmail.com>
2023-11-15 13:11:48 +0100

    updated

--- packages/sh_api/src/utils/projectHelper/generate.ts
+++ packages/sh_api/src/utils/projectHelper/generate.ts
@@ -16,10 +16,18 @@ export default async function ProjectGenerate(
   const codebaseArchitecture = findStack(tech_stacks, "codebase_acrhitecture");
   const _frontend = findStack(tech_stacks, "frontend");
   const _backend = findStack(tech_stacks, "backend");
+  const _design_system = findStack(tech_stacks, "design_system");
+  const _database = findStack(tech_stacks, "database");
+  const _mailing = findStack(tech_stacks, "mailing");
+  const _authentication = findStack(tech_stacks, "authentication");
+  const _payment = findStack(tech_stacks, "payment");
 
   console.log(codebaseArchitecture);
   if (codebaseArchitecture === "monorepo") {
     console.log(formatedName, _frontend, _backend);
+    // if frontend
+    // if backend
+    // if frontend and backend
   }
   if (codebaseArchitecture === "monolith") {
   }
