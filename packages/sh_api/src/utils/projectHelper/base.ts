0095c3ddac4ecd29aa784ce93fa62c30e4e6dc64
Benrobo <alumonabenaiah71@gmail.com>
2023-11-16 00:06:20 +0100

    updated

--- packages/sh_api/src/utils/projectHelper/base.ts
+++ packages/sh_api/src/utils/projectHelper/base.ts
@@ -1,30 +1,118 @@
 import { TEMP_DIR } from "@/config/project";
 import GithubRepoActions from "./repoActions";
-import { createDir } from "@veloz/shared/utils/filemanager";
+import { createDir, createFile } from "@veloz/shared/utils/filemanager";
+import { RootMonorepoPkgJson } from "@/data/package_json";
+import mongoose from "mongoose";
+import { Project } from "@veloz/shared/models";
+
+type MonorepoResp = {
+  frontendPath: string | null;
+  backendPath: string | null;
+  success: boolean;
+};
 
 export default class BaseSetup {
   readonly githubActions;
   constructor() {
     this.githubActions = GithubRepoActions;
   }
-  public async setupMonorepo(proj_name: string) {
+  public setupMonorepo(
+    proj_name: string,
+    _frontend: string | null,
+    _backend: string | null
+  ) {
+    let monorepoResp: MonorepoResp = {
+      frontendPath: null,
+      backendPath: null,
+      success: false,
+    };
     // create the directory based on the name
-    const _dirCreated = createDir(TEMP_DIR, proj_name);
+    const _dirCreated = createDir(TEMP_DIR, proj_name, true);
     if (!_dirCreated.success) {
       console.log(`[Monorepo Setup]: ${_dirCreated.msg}`);
-      return false;
+      return monorepoResp;
     }
 
-    console.log(_dirCreated.path);
-    // init yarn workspaces
-    // setup root package.json
-    // setup different app structure i.e [Just the folder]
-    // shared,app(react, nextjs, [nextjs + next-api]),api (if BE isn't nextjs-api)
-    //
-    // this.githubActions.downloadRepo()
+    // create base packages folder
+    const _pkgsDir = createDir(_dirCreated.path, "packages");
+    if (!_pkgsDir.success) {
+      console.log(`[Monorepo Setup]: ${_dirCreated.msg}`);
+      return monorepoResp;
+    }
+    // create packages readme file
+    createFile(_dirCreated.path, "README.md");
+    createFile(_pkgsDir.path, "README.md");
+
+    // create root pkg.json
+    const _rootPkgJsonContent = RootMonorepoPkgJson.replace(
+      "{{proj_name}}",
+      proj_name
+    ).replace("{{pkg_description}}", "Project scaffolded with veloz.");
+
+    createFile(_dirCreated.path, "package.json", _rootPkgJsonContent, true);
+
+    // create packages (app, server | api)
+    if (_frontend) {
+      // create FE directory
+      const _feCreated = createDir(_pkgsDir.path, "app");
+      if (!_feCreated.success) {
+        console.log(
+          `[Monorepo Setup]: [Failed initializing frontend folder] ${_dirCreated.msg}`
+        );
+        return monorepoResp;
+      }
+
+      monorepoResp["frontendPath"] = _feCreated.path;
+    }
+    if (_frontend && _backend) {
+      // check if backend / frontend isn't nextjs
+      // if it nextjs, then we don't need double folders
+      if (_backend !== "nextjs-api" && _frontend !== "nextjs") {
+        const _feCreated = createDir(_pkgsDir.path, "app");
+        const _beCreated = createDir(_pkgsDir.path, "server");
+
+        if (!_feCreated.success) {
+          console.log(
+            `[Monorepo Setup]: [Failed initializing frontend folder] ${_dirCreated.msg}`
+          );
+          return monorepoResp;
+        }
+        if (!_beCreated.success) {
+          console.log(
+            `[Monorepo Setup]: [Failed initializing backend folder] ${_dirCreated.msg}`
+          );
+          return monorepoResp;
+        }
+
+        monorepoResp["backendPath"] = _beCreated.path;
+        monorepoResp["frontendPath"] = _feCreated.path;
+      } else {
+        console.log(`[Nextjs Client & Server] [Detected]`);
+      }
+    }
+
+    monorepoResp["success"] = true;
+    return monorepoResp;
   }
 
   public async setupMonolith() {}
 
-  private initYarnworkspace() {}
+  public async updateProjectStatus(
+    userId: string,
+    proj_id: string,
+    status: "pending" | "failed" | "done"
+  ) {
+    // UPDATE PROJECT STATUS
+    await Project.updateOne(
+      {
+        uId: userId,
+        _id: new mongoose.Types.ObjectId(proj_id),
+      },
+      {
+        $set: {
+          status,
+        },
+      }
+    );
+  }
 }
