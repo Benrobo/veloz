40ef2c31518628aa029ecc140cb3ff57e4378881
Benrobo <alumonabenaiah71@gmail.com>
2023-11-15 20:30:19 +0100

    updated

---
+++ packages/shared/utils/filemanager.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+
+export function createDir(path: string, name: string) {
+  const dir = `${path}/${name}`;
+  let resp: { success: boolean; msg: string | null } = {
+    success: false,
+    msg: null,
+  };
+  let msg = "";
+  try {
+    // check if path exists
+    if (!fs.existsSync(path)) {
+      msg = `Failed to create ${name} folder, path don't exist.`;
+      resp["msg"] = msg;
+      return resp;
+    }
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+
+    resp["success"] = true;
+    resp["msg"] = null;
+    return resp;
+  } catch (e: any) {
+    msg = `Something went wrong creating ${name} directory: ${e?.message}.`;
+    resp["msg"] = msg;
+    return resp;
+  }
+}
