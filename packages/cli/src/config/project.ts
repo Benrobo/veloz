950dd1571816e7aefbcb65fe73fbd89ef7c0bfb4
Benrobo <alumonabenaiah71@gmail.com>
2023-11-16 12:29:54 +0100

    added major changes

--- packages/cli/src/config/project.ts
+++ packages/cli/src/config/project.ts
@@ -3,10 +3,9 @@ import { fileURLToPath } from "url";
 
 export const SCRIPT_TITLE = "veloz-create";
 
-// export const TmpDir = path.join(
-//   // @ts-expect-error
-//   path.dirname(fileURLToPath(import.meta.url)),
-//   "..",
-//   "templates"
-// );
-export const TEMP_DIR = path?.join(__dirname, "..", "templates");
+export const TEMP_DIR = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "..",
+  "templates"
+);
+// export const TEMP_DIR = path?.join(__dirname, "..", "templates");
