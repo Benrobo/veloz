4818896577145be47af43f0b245e4de2b0814c57
Benrobo <alumonabenaiah71@gmail.com>
2023-11-17 16:42:24 +0100

    updated

---
+++ packages/cli/src/data/repo.ts
@@ -0,0 +1,12 @@
+export const GithubRepo = [
+  {
+    name: "veloz-react",
+    stack: "react",
+    url: "https://github.com/veloz-org/veloz-react.git",
+  },
+  {
+    name: "veloz-next",
+    stack: "nextjs",
+    url: "https://github.com/veloz-org/veloz-nextjs.git",
+  },
+];
