4b2eef5ddb7e1d9e890d7a51ba3b367846ae0314
Benrobo <alumonabenaiah71@gmail.com>
2023-11-17 16:42:44 +0100

    updated

--- packages/cli/src/utils/projectHelper/_frontendSetup.ts
+++ packages/cli/src/utils/projectHelper/_frontendSetup.ts
@@ -3,6 +3,14 @@ import BaseSetup from "./base.js";
 import { spinner } from "@clack/prompts";
 import { sleep } from "../index.js";
 import logger from "../logger.js";
+import GithubRepoActions from "../../utils/projectHelper/repoActions.js";
+import { GithubRepo } from "../../data/repo.js";
+import chalk from "chalk";
+import { createFile } from "../filemanager.js";
+import tailwindcss_config from "../../data/frontend/config/tailwind.config.js";
+import prettify from "../prettier.js";
+
+const githubActions = GithubRepoActions;
 
 type FeProps = {
   fe_tech: string | null;
@@ -55,16 +63,56 @@ export default class _FrontendSetup extends BaseSetup {
 
   async _reactSetup() {
     const s = spinner();
-    const { fe_tech, cb_arch, design_system, auth } = this.props;
+    const { fe_tech, name, cb_arch, design_system, auth, _frontendPath } =
+      this.props;
     try {
-      s.start("Scaffolding frontend..");
+      s.start("Cloning repo...");
       await sleep(1);
 
-      s.stop();
+      const repoUrl = GithubRepo.find((r) => r.stack === fe_tech)?.url;
+      const doneCloning = await githubActions.cloneRepo(_frontendPath, repoUrl);
 
-      // pull github repo
+      if (doneCloning?.success) {
+        s.stop("✅ Done Cloning");
+      } else {
+        s.stop(`❌ ${chalk.redBright(doneCloning?.msg)}`);
+        logger.error(doneCloning?.errMsg);
+        return;
+      }
+
+      // manage package.json first
+      await this.managePkgJson(name, _frontendPath, fe_tech);
+
+      // check if design system is selected
+      if (design_system) {
+        // tailwindcss support
+        if (design_system === "tailwindcss") {
+          await this.initTailwindcss(_frontendPath, fe_tech);
+        }
+      }
     } catch (e: any) {}
   }
 
   async _vanillajsSetup() {}
+
+  async initTailwindcss(base_path: string, fe_stack: string) {
+    if (fe_stack === "react") {
+      tailwindcss_config.content.length = 0;
+      tailwindcss_config.content.push("./src/**/*.{js,jsx,ts,tsx}");
+
+      const modifiedTwContent = `
+      const config = ${JSON.stringify(tailwindcss_config, null, 2)}
+      
+      export default config;
+      `;
+
+      // create tailwind.config.ts file
+      const prettifiedConfig = await prettify(modifiedTwContent, "babel");
+      await createFile(base_path, "tailwind.config.ts", prettifiedConfig);
+    }
+    if (fe_stack === "nextjs") {
+    }
+    if (fe_stack === "vanillajs") {
+    }
+  }
 }
