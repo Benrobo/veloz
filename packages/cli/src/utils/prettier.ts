f4ea34b99d66945d80c9c245c4b13d4bdcbd972c
Benrobo <alumonabenaiah71@gmail.com>
2023-11-17 16:42:42 +0100

    updated

---
+++ packages/cli/src/utils/prettier.ts
@@ -0,0 +1,6 @@
+import prettier from "prettier";
+
+export default async function prettify(code: string, lang: string) {
+  const pretty = await prettier.format(code, { semi: false, parser: lang });
+  return pretty;
+}
