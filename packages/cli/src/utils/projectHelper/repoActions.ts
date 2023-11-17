3fa8d278e438f226d1b8ebf0fded259dc3a14f87
Benrobo <alumonabenaiah71@gmail.com>
2023-11-17 16:42:48 +0100

    updated

--- packages/cli/src/utils/projectHelper/repoActions.ts
+++ packages/cli/src/utils/projectHelper/repoActions.ts
@@ -1,9 +1,28 @@
+import { $ } from "execa";
+import logger from "../logger.js";
+
+let response = {
+  success: false,
+  msg: null,
+  errMsg: null,
+};
 class GithubRepoActions {
-  async cloneRepo(proj_name: string) {}
+  async cloneRepo(path: string, url: string) {
+    try {
+      await $`git clone ${url} ${path}`;
+      response["success"] = true;
+      return response;
+    } catch (e: any) {
+      response["success"] = false;
+      response["msg"] = `Failed cloning repo.`;
+      response["errMsg"] = `Error cloning: ${e.message}`;
+      return response;
+    }
+  }
 
-  async createGhRepo(proj_name: string) {}
+  async createGhRepo(proj_name: string, path: string) {}
 
-  async initGit(rootDir: string) {}
+  async initGit(rootDir: string, path: string) {}
 }
 
 export default new GithubRepoActions();
