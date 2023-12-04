f37d9aaabf256d9c4977b110969f7bb7eec767c4
Benrobo <alumonabenaiah71@gmail.com>
2023-12-04 01:10:13 +0100

    Add rotate_token API endpoint

---
+++ packages/app/src/pages/api/user/rotate_token.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import CatchError from "../lib/error";
+import { isAuthenticated } from "../middlewares/auth";
+import userServices from "../services/user.services";
+import env from "../config/env";
+import { connectDB } from "../lib/utils";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await connectDB(env.MONGO_DB_URL as string);
+
+  if (req.method === "PATCH") {
+    return userServices.rotateToken(req, res);
+  }
+}
+
+export default CatchError(isAuthenticated(handler));
