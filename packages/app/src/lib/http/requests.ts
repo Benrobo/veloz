3572903e713096b82b00b0609040c6fc6dba62b8
Benrobo <alumonabenaiah71@gmail.com>
2023-12-04 01:09:54 +0100

    Add template consumption and token rotation APIs

--- packages/app/src/lib/http/requests.ts
+++ packages/app/src/lib/http/requests.ts
@@ -20,3 +20,14 @@ export const getLastUpdated = async (name: string) => {
   const req = await $axios.get(`/api/github/last_commit?template=${name}`);
   return req.data;
 };
+
+export const getTemplateConsumption = async (name: string) => {
+  const req = await $axios.get(`/api/template/consumption?template=${name}`);
+  return req.data;
+};
+
+// rotate veloz token
+export const rotateToken = async () => {
+  const req = await $axios.patch(`/api/user/rotate_token`);
+  return req.data;
+};
