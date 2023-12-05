093b2d93dc5a5ba9bf3023df731d4d7206d1ef04
Benrobo <alumonabenaiah71@gmail.com>
2023-12-05 01:04:39 +0100

    Add PurchasedItems model

--- packages/app/src/pages/api/models/index.ts
+++ packages/app/src/pages/api/models/index.ts
@@ -1,5 +1,6 @@
 import User from "./users.model";
 import Invites from "./invites.model";
+import PurchasedItems from "./purchasedItems.model";
 import TemplateConsumption from "./template_consumption.model";
 
-export { User, Invites, TemplateConsumption };
+export { User, Invites, PurchasedItems, TemplateConsumption };
