430f0e51c286197f2176179c1c4e734d40c83197
Benrobo <alumonabenaiah71@gmail.com>
2023-12-06 12:02:08 +0100

    Refactor Lemonsqueezy webhook handler

--- packages/app/src/pages/api/webhook/lemonsqueezy.ts
+++ packages/app/src/pages/api/webhook/lemonsqueezy.ts
@@ -4,53 +4,28 @@ import { connectDB } from "../lib/utils";
 import crypto from "crypto";
 import sendResponse from "../lib/sendResponse";
 import { RESPONSE_CODE } from "@veloz/shared/types";
+import { Readable } from "stream";
+import LemonsqueezyWebhookHandler from "../lib/lemonsqueezy";
+
+export const config = {
+  api: {
+    //! This is a must to work, otherwise you might encounter error:
+    bodyParser: false,
+  },
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    // await connectDB(env.MONGO_DB_URL as string);
+  await connectDB(env.MONGO_DB_URL as string);
 
-    const secret = String(process.env.LEMONSQUEEZY_WEBHOOK_SECRET);
-    const rawBody =
-      typeof req.body === "string" ? req.body : JSON.stringify(req.body);
-    const hmac = crypto.createHmac("sha256", secret);
-    const digest = Buffer.from(hmac.update(rawBody).digest("hex"), "utf8");
-    const signature = Buffer.from(
-      String(req.headers["x-signature"]) || "",
-      "utf8"
-    );
+  const secret = String(process.env.LEMONSQUEEZY_WEBHOOK_SECRET);
+  const { data, errorMsg, success } = await LemonsqueezyWebhookHandler(
+    req,
+    res,
+    secret
+  );
 
-    console.log({
-      digest: digest.toString(),
-      signature: signature.toString(),
-      secret,
-      //   header: String(req.headers["x-signature"]),
-    });
-
-    let sigOk = false;
-
-    try {
-      sigOk = crypto.timingSafeEqual(digest, signature);
-    } catch (e: any) {
-      console.log(`❌ ERROR: timingSafeEqual failed: ${e.message}`);
-    }
-
-    if (!sigOk) {
-      console.log(`❌ ERROR: Invalid signature`);
-      sendResponse.error(
-        res,
-        RESPONSE_CODE.FORBIDDEN,
-        "❌ ERROR: Invalid signature",
-        403
-      );
-      return;
-    }
-
-    // Parse the webhook's data as JSON
-    const event = req.body;
-
-    console.log(event);
-  }
+  console.log("data", data);
 }
