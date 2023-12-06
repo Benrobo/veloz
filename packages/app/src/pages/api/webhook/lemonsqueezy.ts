59456afe631fab5d23a97e66dcfd736a9d300698
Benrobo <alumonabenaiah71@gmail.com>
2023-12-06 09:08:22 +0100

    Refactor webhook signature verification logic

--- packages/app/src/pages/api/webhook/lemonsqueezy.ts
+++ packages/app/src/pages/api/webhook/lemonsqueezy.ts
@@ -9,37 +9,48 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await connectDB(env.MONGO_DB_URL as string);
+  if (req.method === "POST") {
+    // await connectDB(env.MONGO_DB_URL as string);
 
-  const rawBody = req.body.toString();
-  const secret = String(process.env.LEMONSQUEEZY_WEBHOOK_SECRET);
-  const headerSignature = String(req.headers["x-signature"]) ?? "";
-
-  const hmac = crypto.createHmac("sha256", secret);
-  const digest = Buffer.from(hmac.update(rawBody).digest("hex"), "utf8");
-  const signature = Buffer.from(headerSignature, "utf8");
-
-  let sigOk = false;
-
-  try {
-    sigOk = crypto.timingSafeEqual(digest, signature);
-  } catch (e: any) {
-    console.log(`❌ ERROR: timingSafeEqual failed: ${e.message}`);
-  }
-
-  if (!sigOk) {
-    console.log(`❌ ERROR: Invalid signature`);
-    sendResponse.error(
-      res,
-      RESPONSE_CODE.FORBIDDEN,
-      "❌ ERROR: Invalid signature",
-      403
+    const secret = String(process.env.LEMONSQUEEZY_WEBHOOK_SECRET);
+    const rawBody =
+      typeof req.body === "string" ? req.body : JSON.stringify(req.body);
+    const hmac = crypto.createHmac("sha256", secret);
+    const digest = Buffer.from(hmac.update(rawBody).digest("hex"), "utf8");
+    const signature = Buffer.from(
+      String(req.headers["x-signature"]) || "",
+      "utf8"
     );
-    return;
+
+    console.log({
+      digest: digest.toString(),
+      signature: signature.toString(),
+      secret,
+      //   header: String(req.headers["x-signature"]),
+    });
+
+    let sigOk = false;
+
+    try {
+      sigOk = crypto.timingSafeEqual(digest, signature);
+    } catch (e: any) {
+      console.log(`❌ ERROR: timingSafeEqual failed: ${e.message}`);
+    }
+
+    if (!sigOk) {
+      console.log(`❌ ERROR: Invalid signature`);
+      sendResponse.error(
+        res,
+        RESPONSE_CODE.FORBIDDEN,
+        "❌ ERROR: Invalid signature",
+        403
+      );
+      return;
+    }
+
+    // Parse the webhook's data as JSON
+    const event = req.body;
+
+    console.log(event);
   }
-
-  // Parse the webhook's data as JSON
-  const event = JSON.parse(rawBody);
-
-  console.log(event);
 }
