1628f466b11a3a209d0039528d4d9cd0be5ba788
Benrobo <alumonabenaiah71@gmail.com>
2023-12-09 18:22:35 +0100

    upadated

--- packages/app/src/components/Why.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FlexColCenter } from "./Flex";
-
-function Why() {
-  return <FlexColCenter className="w-full h-full"></FlexColCenter>;
-}
-
-export default Why;
