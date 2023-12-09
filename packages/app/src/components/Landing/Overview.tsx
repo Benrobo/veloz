dcd4b26203723a5a60c9ce9760d02e3cd0a9a30b
Benrobo <alumonabenaiah71@gmail.com>
2023-12-09 18:31:38 +0100

    updated

--- packages/app/src/components/Landing/Overview.tsx
+++ packages/app/src/components/Landing/Overview.tsx
@@ -44,43 +44,45 @@ const overviewData = [
 function Overview() {
   return (
     <FlexColStart className="relative w-full h-auto bg-dark-102 border-t-solid border-t-[1px] border-t-gray-100/30 px-9 py-[6em]">
-      <div className="w-full md:max-w-[95%] mx-auto h-auto grid grid-cols-1 md:grid-cols-2 gap-10 px-5">
-        <FlexColStart className="w-full h-auto">
-          <FlexRowStartCenter className="w-auto">
-            <Flame
-              size={50}
-              className="p-3 bg-red-305 text-white-100 rounded-lg"
+      <FlexColCenter className="md:max-w-[95%] xl:max-w-[85%] mx-auto">
+        <div className="w-full h-auto grid grid-cols-1 md:grid-cols-2 gap-10 px-5">
+          <FlexColStart className="w-full h-auto">
+            <FlexRowStartCenter className="w-auto">
+              <Flame
+                size={50}
+                className="p-3 bg-red-305 text-white-100 rounded-lg"
+              />
+              <h1 className="text-white-100 font-ppSB">
+                Get Started in minutes.
+              </h1>
+            </FlexRowStartCenter>
+            <p className="text-white-300/40 font-ppSB text-2xl md:text-4xl">
+              <span className="text-white-100">Launch with Confidence</span>.
+              Our SaaS Starter Kit is equipped with everything you need for a
+              seamless project kickstart."
+            </p>
+          </FlexColStart>
+          <FlexColCenter className="w-full">
+            <Image
+              src="/images/screenshots/scr1.png"
+              alt="overview"
+              className="w-full h-auto rounded-lg shadow-xl shadow-dark-105"
+              width={0}
+              height={0}
             />
-            <h1 className="text-white-100 font-ppSB">
-              Get Started in minutes.
-            </h1>
-          </FlexRowStartCenter>
-          <p className="text-white-300/40 font-ppSB text-2xl md:text-4xl">
-            <span className="text-white-100">Launch with Confidence</span>. Our
-            SaaS Starter Kit is equipped with everything you need for a seamless
-            project kickstart."
-          </p>
-        </FlexColStart>
-        <FlexColCenter className="w-full">
-          <Image
-            src="/images/screenshots/scr1.png"
-            alt="overview"
-            className="w-full h-auto rounded-lg shadow-xl shadow-dark-105"
-            width={0}
-            height={0}
-          />
-        </FlexColCenter>
-      </div>
-      <FlexRowStartBtw className="w-full md:max-w-[95%] mx-auto h-auto mt-20 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 ">
-        {overviewData.map((data, i) => (
-          <OverviewCard
-            title={data.title}
-            _key={data.key}
-            description={data.desc}
-            key={i}
-          />
-        ))}
-      </FlexRowStartBtw>
+          </FlexColCenter>
+        </div>
+        <FlexRowStartBtw className="w-full h-auto mt-20 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 ">
+          {overviewData.map((data, i) => (
+            <OverviewCard
+              title={data.title}
+              _key={data.key}
+              description={data.desc}
+              key={i}
+            />
+          ))}
+        </FlexRowStartBtw>
+      </FlexColCenter>
     </FlexColStart>
   );
 }
