d526214634db94db6fb3f7ec615d142683430f44
Benrobo <alumonabenaiah71@gmail.com>
2023-12-10 13:18:52 +0100

    Update KitHeader component styling

--- packages/app/src/components/Header/KitHeader.tsx
+++ packages/app/src/components/Header/KitHeader.tsx
@@ -23,8 +23,9 @@ function KitHeader({ name }: Props) {
   return (
     <FlexRowStart className="relative w-full h-full gap-2 py-[4em]">
       <BlurBgRadial className=" w-[100%] lg:w-[60%] h-[300px] absolute top-[10%] lg:top-[-10%] bg-white-300/10 " />
-      <div className="w-auto h-auto max-w-[90%] mx-auto mt-9 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 ">
-        <FlexColStart className="w-full">
+      {/* Header */}
+      <div className="relative w-auto h-auto max-w-[95%] md:max-w-[90%] mx-auto py-[5em] flex flex-col-reverse md:grid md:grid-cols-2 gap-10 pattern-bg overflow-y-hidden ">
+        <FlexColStart className="w-full px-3 z-[100]">
           <h1 className="text-white-100 font-ppEB text-2xl md:text-4xl">
             Zeus Kits
           </h1>
