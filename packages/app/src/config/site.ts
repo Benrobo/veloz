4b846f4bf514ee9a944a14b68afb72d3d6d30b2e
Benaiah <alumonabenaiah71@gmail.com>
2024-01-12 15:14:57 +0100

    Update Veloz documentation site configuration

--- packages/app/src/config/site.ts
+++ packages/app/src/config/site.ts
@@ -7,7 +7,7 @@ const siteConfig = {
   headline: "Veloz Documentation",
   description: "Your saas launchpad",
   keywords: "boilerplate, starter kit, saucecode",
-  image: "/images/og/og-image.png",
+  image: "/images/og-image.png",
 };
 
 export default siteConfig;
